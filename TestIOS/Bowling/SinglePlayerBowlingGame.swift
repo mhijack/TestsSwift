21e4a8c6484f8b098b472691093ef98e48e00db9
Jianyuan Chen <jianyuan.chen12@gmail.com>
2021-04-03 15:32:57 -0700

    Extract Scorer class

--- TestIOS/Bowling/SinglePlayerBowlingGame.swift
+++ TestIOS/Bowling/SinglePlayerBowlingGame.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-class SinglePlayerBowlingGame {
+class Scorer {
     
-    private var allThrows: [Int] = Array.init(repeating: 0, count: 21)
     private var nextThrowIndex: Int = 0
+    private var allThrows: [Int] = Array.init(repeating: 0, count: 21)
     
-    func score() -> Int {
+    public func scoreForFrame(frameIndex: Int) -> Int {
         var score: Int = 0
         var frameIndex: Int = 0
         for _ in 0..<10 {
@@ -30,14 +30,14 @@ class SinglePlayerBowlingGame {
         return score
     }
     
-    func roll(_ pins: Int) {
+    public func addThrow(_ pins: Int) {
         allThrows[nextThrowIndex] = pins
         nextThrowIndex += 1
     }
     
 }
 
-extension SinglePlayerBowlingGame {
+extension Scorer {
     
     /// Returns if current frame in the game constitutes a spare
     /// - Parameter frameIndex:
@@ -49,4 +49,26 @@ extension SinglePlayerBowlingGame {
     private func isStrike(at frame: Int) -> Bool {
         return allThrows[frame] == 10
     }
+    
+}
+
+class SinglePlayerBowlingGame {
+    
+    private var currentFrame: Int = 0
+    private var firstThrowInframe: Bool = true
+    
+    private var scorer: Scorer = Scorer()
+    
+    func score() -> Int {
+        return scorer.scoreForFrame(frameIndex: currentFrame)
+    }
+    
+    func roll(_ pins: Int) {
+        scorer.addThrow(pins)
+    }
+    
+}
+
+extension SinglePlayerBowlingGame {
+    
 }
